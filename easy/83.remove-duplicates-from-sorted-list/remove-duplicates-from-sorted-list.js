fba506f430ceac39f8d4ee4bbc7f8a8ed9210f26
新舰同学Xekin <xekin2017@Gmail.com>
2019-08-12 00:36:49 +0800

    update 83

--- easy/83.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ easy/83.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -14,6 +14,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// while 遍历
 var deleteDuplicates = function(head) {
     if (!head) return null
     var self = head
@@ -25,5 +26,22 @@ var deleteDuplicates = function(head) {
         } else self.next = null
     }
     return head
+};
+
+// 递归
+var deleteDuplicates = function(head) {
+    if (!head) return null
+    var mNode = new ListNode(head.val)
+    var self = mNode
+    function check(node, next) {
+        if (next === null) return
+        else if (node.val !== next.val) {
+            self.next = next
+            self = self.next
+        } else self.next = null
+        check(next, next.next)
+    }
+    check(head, head.next)
+    return mNode
 };
 
